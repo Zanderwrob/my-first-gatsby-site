b045378da2ed9fd0354341e511f6749ddd2b54b5
Zanderwrob <zanderwrob@gmail.com>
2021-08-07 23:26:09 -0500

    Added a picture to the Home Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../../components/layout'
-
+import { StaticImage } from 'gatsby-plugin-image'
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <main>
       <Layout pageTitle="Home Page">
         <p>Da Home Page :D</p>
+        <StaticImage
+        alt="Wild Scrible"
+        src="https://i.pinimg.com/236x/ff/81/2d/ff812df7e425bcff4fb92e578e139f83.jpg"
+        />
       </Layout>
     </main>
   )
